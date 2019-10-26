8fcc9203a293bbb61f2c74b56ef6b940b0184b9c
Randy Steele <steelerx155@gmail.com>
2019-10-26 17:53:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+   self.end_with?("?")
   end
 
   def exclamation?
-
+   self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+ count_sentences = self.split(/[.?!]+/)
+# binding.pry
+count_sentences.count
+    end
 end
